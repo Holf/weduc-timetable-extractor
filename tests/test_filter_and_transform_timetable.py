cee25501b178541bdef4f531d2c9dac167ad3e1b
Holf <holf@holf.net>
2024-10-01 10:06:50 +0100

    test: add time freeze to timetable transform test

--- tests/test_filter_and_transform_timetable.py
+++ tests/test_filter_and_transform_timetable.py
@@ -1,8 +1,11 @@
 import json
 
+from freezegun import freeze_time
+
 from weduc_timetable_extractor.weduc_interaction import filter_and_transform_timetable
 
 
+@freeze_time("2024-09-15")
 def test_filter_and_transform_timetable(snapshot):
 
     with open("./tests/assets/weduc_response.json", "r") as file:
